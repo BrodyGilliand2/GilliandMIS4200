fd8634437cbab7d9cb024433847e16af9f4f07ca
Gilliand <bg741916@ohio.edu>
2020-09-20 12:07:27 -0400

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GilliandMIS4200.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GilliandMIS4200.Startup))]
+namespace GilliandMIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
